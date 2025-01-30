797f9744508c50a01bc2e403b4968532e829a896
Aditya Sharma <sharma.aditya19@outlook.com>
2025-01-30 21:11:51 +0530

    Update 1_SelectionSort.cpp

--- Day1/Algorithms/1_SelectionSort.cpp
+++ Day1/Algorithms/1_SelectionSort.cpp
@@ -1,1 +1,13 @@
-//first
+class Solution {
+public:
+    vector<int> selectionSort(vector<int>& nums) {
+        for(int i=0;i<(nums.size()-1);i++){
+            int indexOfSmallest = i;
+            for(int j=i+1;j<nums.size();j++)
+                if(nums[j] < nums[indexOfSmallest])
+                    indexOfSmallest = j;
+            swap(nums[i],nums[indexOfSmallest]);
+        }
+        return nums;
+    }
+};
